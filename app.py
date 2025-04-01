0e014d7cc3f8e90b55d22ddef5e535167c016934
sokatako <167274686+sokatako@users.noreply.github.com>
2025-04-01 22:51:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template, url_for
+# from flask import Flask, render_template, url_for
+
+# app = Flask(__name__)
+
+
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0")
+
+from flask import Flask, render_template, url_for, send_from_directory
+import os
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route('/bgm/<path:filename>')
+def serve_audio(filename):
+    return send_from_directory('static/bgm', filename)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-
