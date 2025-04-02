b4190e136deefb257ef7b274485f4a0b3f72089a
sokatako <167274686+sokatako@users.noreply.github.com>
2025-04-02 15:09:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ import os
 
 app = Flask(__name__)
 
+# 根路由，返回主頁
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# 靜態音樂文件處理
 @app.route('/bgm/<path:filename>')
 def serve_audio(filename):
-    return send_from_directory('static/bgm', filename)
+    return send_from_directory(os.path.join(app.root_path, 'static', 'bgm'), filename)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Azure會設置PORT變數
-    app.run(host="0.0.0.0", port=port)
+    # Flask 運行在 0.0.0.0，這樣它可以接收來自外部的請求
+    app.run(host="0.0.0.0", port=5000)
+
